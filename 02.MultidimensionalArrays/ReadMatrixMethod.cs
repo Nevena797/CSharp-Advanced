a9c983a8e4f6ed547d18b5b278ff8afe627d2851
Nevena <nevena.rogova@gmx.at>
2025-05-19 11:15:20 +0300

    Add files via upload

---
+++ 02.MultidimensionalArrays/ReadMatrixMethod.cs
@@ -0,0 +1,10 @@
+﻿namespace ReadMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
