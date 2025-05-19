5c0b8334786df1aa66da3f5ec7061bafa6795ff9
Nevena <nevena.rogova@gmx.at>
2025-05-19 10:36:08 +0300

    Add files via upload

---
+++ 02.MultidimensionalArrays/02.SumMatrixCols.cs
@@ -0,0 +1,28 @@
+﻿namespace _02.SumMatrixRows
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] matrix = new int[3, 3]
+            {
+                {1,2,3 },
+                {4,5,6 },
+                {7,8,9 }
+            };
+            for (int col= 0; col < matrix.GetLength(0); col++)
+            {
+                int sum = 0;
+
+                for (int row = 0; row < matrix.GetLength(1); row++)
+                {
+                    sum += matrix[row, col];
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
