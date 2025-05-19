85d10610f2aa279fe738e1047bf8fd31a4b3c8f4
Nevena <nevena.rogova@gmx.at>
2025-05-19 09:50:31 +0300

    Add files via upload

---
+++ 02.MultidimensionalArrays/01.SumMatrixElements.cs
@@ -0,0 +1,40 @@
+﻿namespace _01.SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lenght = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = lenght[0];
+            int cows = lenght[1];
+
+            int[,] matrix = new int[rows, cows];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] line = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cows; col++)
+                {
+                    matrix[row,col] = line[col];
+                }
+            }
+
+            int sum = 0;
+            for (int row = 0; row < rows; row++)
+            {
+
+                for (int col = 0; col < cows; col++)
+                {
+                    sum += matrix[row,col];
+                }
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cows);
+            Console.WriteLine(sum);
+
+        }
+    }
+}
