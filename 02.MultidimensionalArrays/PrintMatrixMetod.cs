d84f45ce26fbf13db6bc175123b2a86d49a95539
Nevena <nevena.rogova@gmx.at>
2025-05-19 11:26:16 +0300

    Add files via upload

---
+++ 02.MultidimensionalArrays/PrintMatrixMetod.cs
@@ -0,0 +1,22 @@
+﻿void PrintMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            if (matrix[row, col] < 10)
+            {
+                Console.Write($"  {matrix[row, col]} ");
+            }
+            else if (matrix[row, col] < 100)
+            {
+                Console.Write($" {matrix[row, col]} ");
+            }
+            else
+            {
+                Console.Write(matrix[row, col] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
