2989a8025376b34113bba635174c6c30d264447e
Nevena <nevena.rogova@gmx.at>
2025-06-01 08:19:30 +0300

    Add files via upload

---
+++ 02.MultidimensionalArrays/03.PrimaryDiagonal/03.PrimaryDiagonal.cs
@@ -0,0 +1,32 @@
+﻿namespace _3.PrimaryDiagonal
+{
+class PrimaryDiagonalSum
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            // Read the matrix //3
+            for (int row = 0; row < n; row++)
+            {
+                
+                string[] input = Console.ReadLine().Split(); //["11", "2", "4"] - string array
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = int.Parse(input[col]); 
+                }
+            }
+
+            int sum = 0;
+
+            // Sum the primary diagonal
+            for (int i = 0; i < n; i++)
+            {
+                sum += matrix[i, i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
