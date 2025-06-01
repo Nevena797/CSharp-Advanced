e9ec55cec1be8db49b3dfb146d45549e61d91df5
Nevena <nevena.rogova@gmx.at>
2025-06-01 22:46:13 +0300

    Add files via upload

---
+++ 02.Multidimensional ArraysExercise/01. DiagonalDifference/01. DiagonalDifference.cs
@@ -0,0 +1,39 @@
+﻿namespace _01._DiagonalDifference
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            // read the matrix
+            for (int row = 0; row < n; row++)
+            {
+                int[] numbers = Console.ReadLine() //"11 2 4"
+                                       .Split(' ')//"11" "2" "4"
+                                       .Select(int.Parse) //11 2 4
+                                       .ToArray();// [11 2 4]
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+
+            int primarySum = 0;
+            int secondarySum = 0;
+
+            // sum diagonals
+            for (int i = 0; i < n; i++)
+            {
+                primarySum += matrix[i, i];              // (0,0), (1,1), (2,2)
+                secondarySum += matrix[i, n - 1 - i];   // (0,2), (1,1), (2,0)
+            }
+
+            int difference = Math.Abs(primarySum - secondarySum);
+            Console.WriteLine(difference);
+        }
+    }
+}
---
+++ 02.Multidimensional ArraysExercise/02.SquaresInMatrix/02.SquaresInMatrix.cs
@@ -0,0 +1,44 @@
+﻿namespace _02.SquaresInMatrix
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] dimensions = Console.ReadLine().Split(' ');
+            int rows = int.Parse(dimensions[0]);
+            int cols = int.Parse(dimensions[1]);
+
+            char[,] matrix = new char[rows, cols];
+
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)[0].ToCharArray();
+                string[] symbols = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = symbols[col][0];
+                }
+            }
+
+            int squareCount = 0;
+
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    char current = matrix[row, col];
+
+                    if (current == matrix[row, col + 1] &&
+                        current == matrix[row + 1, col] &&
+                        current == matrix[row + 1, col + 1])
+                    {
+                        squareCount++;
+                    }
+                }
+            }
+            Console.WriteLine(squareCount);
+        }
+    }
+}
