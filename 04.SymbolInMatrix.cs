018346ee8be7e82356e6045eda9d2b58d58b590d
Nevena <nevena.rogova@gmx.at>
2025-06-01 12:56:06 +0300

    Add files via upload

---
+++ 04.SymbolInMatrix.cs
@@ -0,0 +1,46 @@
+﻿namespace _04.SymbolInMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            // Read matrix rows
+            for (int i = 0; i < n; i++)
+            {
+                string row = Console.ReadLine();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = row[j];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+            bool found = false;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i, j] == symbol)
+                    {
+                        Console.WriteLine($"({i}, {j})");
+                        found = true;
+                        break;
+                    }
+                }
+                if (found)
+                {
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+        }
+    }
+}
