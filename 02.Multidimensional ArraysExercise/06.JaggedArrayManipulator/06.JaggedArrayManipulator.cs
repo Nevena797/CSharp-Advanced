bff9429650be2b8462270b0fc226c6b7b388af2a
Nevena <nevena.rogova@gmx.at>
2025-06-02 23:09:52 +0300

    Add files via upload

---
+++ 02.Multidimensional ArraysExercise/06.JaggedArrayManipulator/06.JaggedArrayManipulator.cs
@@ -0,0 +1,77 @@
+﻿using System.Data;
+
+namespace _06.JaggedArrayManipulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[][] jagged = new double[n][];
+
+
+            for (int row = 0; row < n; row++)
+            {
+                jagged[row] = Console.ReadLine() //Input: 10 20 30 //jagged[0] = [10, 20, 30]
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(double.Parse)
+                    .ToArray();
+            }
+
+
+            for (int row = 0; row < n - 1; row++)
+            {
+                if (jagged[row].Length == jagged[row + 1].Length)
+                {
+                    for (int col = 0; col < jagged[row].Length; col++)
+                    {
+                        jagged[row][col] *= 2;
+                        jagged[row + 1][col] *= 2;
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < jagged[row].Length; col++)
+                    {
+                        jagged[row][col] /= 2;
+                    }
+                    for (int col = 0; col < jagged[row + 1].Length; col++)
+                    {
+                        jagged[row + 1][col] /= 2;
+                    }
+                }
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] parts = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string action = parts[0];
+                int row = int.Parse(parts[1]);
+                int col = int.Parse(parts[2]);
+                int value = int.Parse(parts[3]);
+
+                if (row >= 0 && row < n && col >= 0 && col < jagged[row].Length)
+                {
+                    if (action == "Add")
+                    {
+                        jagged[row][col] += value;
+                    }
+                    else if (action == "Subtract")
+                    {
+                        jagged[row][col] -= value;
+                    }
+                }
+            }
+
+            for (int row = 0; row < jagged.Length; row++)
+            {
+                for (int col = 0; col < jagged[row].Length; col++)
+                {
+                    Console.Write((int)jagged[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 02.Multidimensional ArraysExercise/07.KnightGame/07.KnightGame.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace KnightGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] board = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string line = Console.ReadLine();
+                for (int col = 0; col < n; col++)
+                {
+                    board[row, col] = line[col];
+                }
+            }
+
+            int removedKnights = 0;
+
+
+            int[] rowMoves = { -2, -2, -1, -1, 1, 1, 2, 2 };
+            int[] colMoves = { -1, 1, -2, 2, -2, 2, -1, 1 };
+
+            while (true)
+            {
+                int maxAttacks = 0;
+                int maxRow = -1;
+                int maxCol = -1;
+
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        if (board[row, col] == 'K')
+                        {
+                            int currentAttacks = CountAttacks(board, row, col, rowMoves, colMoves, n);
+                            if (currentAttacks > maxAttacks)
+                            {
+                                maxAttacks = currentAttacks;
+                                maxRow = row;
+                                maxCol = col;
+                            }
+                        }
+                    }
+                }
+
+                if (maxAttacks == 0)
+                {
+
+                    break;
+                }
+
+
+                board[maxRow, maxCol] = '0';
+                removedKnights++;
+            }
+
+            Console.WriteLine(removedKnights);
+        }
+
+        static int CountAttacks(char[,] board, int row, int col, int[] rowMoves, int[] colMoves, int size)
+        {
+            int attacks = 0;
+
+            for (int i = 0; i < rowMoves.Length; i++)
+            {
+                int targetRow = row + rowMoves[i];
+                int targetCol = col + colMoves[i];
+
+                if (IsInBounds(targetRow, targetCol, size) && board[targetRow, targetCol] == 'K')
+                {
+                    attacks++;
+                }
+            }
+
+            return attacks;
+        }
+
+        static bool IsInBounds(int row, int col, int size)
+        {
+            return row >= 0 && row < size && col >= 0 && col < size;
+        }
+    }
+}
