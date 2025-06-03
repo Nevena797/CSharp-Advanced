1dcfe77b6b48e1e3d227112b0ab5c0860e3f58a9
Nevena <nevena.rogova@gmx.at>
2025-06-03 11:05:43 +0300

    Add files via upload

---
+++ 02.Multidimensional ArraysExercise/08.Bombs/08.Bombs.cs
@@ -0,0 +1,79 @@
+﻿using System.Data;
+
+namespace _08.Bombs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] row = Console.ReadLine()
+                                   .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = row[j];
+                }
+            }
+            string[] bombs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string bomb in bombs)
+            {
+                int[] coords = bomb.Split(',').Select(int.Parse).ToArray();
+                int row = coords[0];
+                int col = coords[1];
+
+                int bombValue = matrix[row, col];
+                if (bombValue <= 0)
+                {
+                    continue;
+                }
+                for (int i = row - 1; i <= row + 1; i++)
+                {
+                    for (int j = col - 1; j <= col + 1; j++)
+                    {
+                        if (i >= 0 && i < n && j >= 0 && j < n)
+                        {
+                            if (matrix[i, j] > 0)
+                            {
+                                matrix[i, j] -= bombValue;
+                            }
+                        }
+                    }
+                }
+                matrix[row, col] = 0;
+            }
+            int aliveCells = 0;
+            int sumAlive = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i, j] > 0)
+                    {
+                        aliveCells++;
+                        sumAlive += matrix[i, j];
+                    }
+                }
+            }
+            Console.WriteLine($"Alive cells: {aliveCells}");
+            Console.WriteLine($"Sum: {sumAlive}");
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 02.Multidimensional ArraysExercise/09.Miner/09.Miner.cs
@@ -0,0 +1,78 @@
+﻿namespace _09.Miner
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+            string[] commands = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            char[,] field = new char[size, size];
+            int totalCoals = 0;
+            int minerRow = 0;
+            int minerCol = 0;
+
+
+            for (int row = 0; row < size; row++)
+            {
+                char[] line = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < size; col++)
+                {
+                    field[row, col] = line[col];
+                    if (field[row, col] == 's')
+                    {
+                        minerRow = row;
+                        minerCol = col;
+                    }
+                    else if (field[row, col] == 'c')
+                    {
+                        totalCoals++;
+                    }
+                }
+            }
+
+            int collectedCoals = 0;
+
+            foreach (var command in commands)
+            {
+                int nextRow = minerRow;
+                int nextCol = minerCol;
+
+                switch (command)
+                {
+                    case "up": nextRow--; break;
+                    case "down": nextRow++; break;
+                    case "left": nextCol--; break;
+                    case "right": nextCol++; break;
+                }
+
+
+                if (nextRow < 0 || nextRow >= size || nextCol < 0 || nextCol >= size)
+                {
+                    continue;
+                }
+
+                minerRow = nextRow;
+                minerCol = nextCol;
+
+                if (field[minerRow, minerCol] == 'c')
+                {
+                    collectedCoals++;
+                    field[minerRow, minerCol] = '*';
+                    if (collectedCoals == totalCoals)
+                    {
+                        Console.WriteLine($"You collected all coals! ({minerRow}, {minerCol})");
+                        return;
+                    }
+                }
+                else if (field[minerRow, minerCol] == 'e')
+                {
+                    Console.WriteLine($"Game over! ({minerRow}, {minerCol})");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{totalCoals - collectedCoals} coals left. ({minerRow}, {minerCol})");
+        }
+    }
+}
